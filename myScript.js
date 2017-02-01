338ff0a4c32ce20cd3dd3b60c7c4d31233b763a4
David Evans <devans6915@aol.com>
2017-02-01 14:48:18 +0000

    Add files via upload

--- myScript.js
+++ myScript.js
@@ -1,59 +1,14 @@
+var myIndex = 0;
+carousel();
 
-
-<button onclick="myFunction()">Try it</button>
-
-<div id="myDIV">
-This is my DIV element.
-</div>
-
-<p><b>Note:</b> The element will not take up any space when the display property set to "none".</p>
-
-<script>
-function myFunction() {
-    var x = document.getElementById('myDIV');
-    if (x.style.display === 'none') {
-        x.style.display = 'block';
-    } else {
-        x.style.display = 'none';
+function carousel() {
+    var i;
+    var x = document.getElementsByClassName("mySlides");
+    for (i = 0; i < x.length; i++) {
+       x[i].style.display = "none";  
     }
+    myIndex++;
+    if (myIndex > x.length) {myIndex = 1}    
+    x[myIndex-1].style.display = "block";  
+    setTimeout(carousel, 2000); // Change image every 2 seconds
 }
-</script>
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-/* When the user clicks on the button, 
-toggle between hiding and showing the dropdown content 
-
-
-function myFunction() {
-    document.getElementById("dropdown").classList.toggle("show");
-}
-
-Close the dropdown if the user clicks outside of it
-window.onclick = function(e) {
-  if (!e.target.matches('.dropdown-content')) {
-
-    var dropdowns = document.getElementsByClassName("dropdown-content");
-    for (var d = 0; d < dropdowns.length; d++) {
-      var openDropdown = dropdowns[d];
-      if (openDropdown.classList.contains('show')) {
-        openDropdown.classList.remove('show');
-      }
-    }
-  }
-}
-*/
