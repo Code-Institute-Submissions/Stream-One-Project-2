bd17df6377618de1c42cda10a0ad4c0c929e755f
David Evans <devans6915@aol.com>
2017-02-24 18:27:16 +0000

    Add files via upload

--- myScript.js
+++ myScript.js
@@ -1,3 +1,13 @@
+angular.module('slideShowExample', ['simple-slideshow'])
+	.controller('slideShowCtrl', ['$scope', function($scope){
+		$scope.slides = [
+			{name: 'Not my cat.', url: 'https://farm2.staticflickr.com/1318/5114665665_e55b2c2169_n.jpg'},
+			{name: 'Again, not my cat.', url: 'https://farm2.staticflickr.com/1079/901626554_8bc51ec160_n.jpg'}]
+	}]);
+
+
+
+
 var myIndex = 0;
 carousel();
 
@@ -12,3 +22,5 @@ function carousel() {
     x[myIndex-1].style.display = "block";  
     setTimeout(carousel, 2000); // Change image every 2 seconds
 }
+
+
