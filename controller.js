e7000aed2958eaa5c226c4cb48dc9c6c7afaa38e
David Evans <devans6915@aol.com>
2017-04-20 10:57:01 +0100

    Add files via upload

--- controller.js
+++ controller.js
@@ -22,7 +22,15 @@ angular.module('RouteControllers', [])
  
             console.log($scope.registrationUser.username + " " + $scope.registrationUser.password);
         };
+
+
+
+
+Thank you for your request, your form has been submitted;
+
+
+
+
+
     });
-
-
 
--- myScript.js
+++ myScript.js
@@ -1,26 +1,43 @@
-angular.module('slideShowExample', ['simple-slideshow'])
-	.controller('slideShowCtrl', ['$scope', function($scope){
-		$scope.slides = [
-			{name: 'Not my cat.', url: 'https://farm2.staticflickr.com/1318/5114665665_e55b2c2169_n.jpg'},
-			{name: 'Again, not my cat.', url: 'https://farm2.staticflickr.com/1079/901626554_8bc51ec160_n.jpg'}]
-	}]);
-
-
-
-
 var myIndex = 0;
 carousel();
 
 function carousel() {
     var i;
-    var x = document.getElementsByClassName("mySlides");
+    var x = document.getElementsByClassName("myCarousel");
     for (i = 0; i < x.length; i++) {
        x[i].style.display = "none";  
     }
     myIndex++;
     if (myIndex > x.length) {myIndex = 1}    
-    x[myIndex-1].style.display = "block";  
+    x[myIndex-1].style.display = "block";
     setTimeout(carousel, 2000); // Change image every 2 seconds
 }
+
+
+
+/************
+Confirmation of form submission
+************/
+
+if (submit clicked) {}
+
+<h3>Thank you for your request, your form has been submitted</h3>
+}
+
+
+
+if (condition1) {
+    block of code to be executed if condition1 is true
+} else if (condition2) {
+    block of code to be executed if the condition1 is false and condition2 is true
+
+
+form.submit({
+  success: function() {
+  	$('#message').html('<p>Thank you for your request.</p>');
+    // code here
+  }
+});
+
 
 
